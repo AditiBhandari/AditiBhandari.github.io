b5abbece55ab62fc67f4edb245b35651945ef6e3
AditiBhandari <aditibhandari2017@u.northwestern.edu>
2015-12-04 09:51:05 -0600

    almost done

--- projects/podcasting/script.js
+++ projects/podcasting/script.js
@@ -1,5 +1,7 @@
-$( "#audio-text" ).click(function() {
-  $( "#transcript" ).toggle( "fast", function() {
-    // Animation complete.
-  });
+$(document).ready(function(){
+	$( "#audio-text" ).click(function() {
+	  $( "#transcript" ).toggle( "fast", function() {
+	    // Animation complete.
+	  });
+	});
 });
